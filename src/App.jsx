893e21dcca32623ddfb684c5b50829bdf8c9e952
simbu07 <silambarasankadhiravan4@gmail.com>
2024-07-23 15:02:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GetCharacterComponent from './Components/CharacterComponent/GetCharacterComponent'
+
+const App = () => {
+  return (
+    <GetCharacterComponent/>
+  )
+}
+
+export default App
